428b36f3126221dc3687f5a995a1d6d8f4bbeb52
Kaito Hayashi <1122hkaito@gmail.com>
2026-05-30 19:10:49 +0900

    final

--- init-db.sql
+++
@@ -1,7 +0,0 @@
--- Hive用
-CREATE USER hiveuser WITH PASSWORD 'hivepassword';
-CREATE DATABASE hive_meta OWNER hiveuser;
-
--- Ranger用
-CREATE USER rangeruser WITH PASSWORD 'rangerpassword';
-CREATE DATABASE ranger_db OWNER rangeruser;
